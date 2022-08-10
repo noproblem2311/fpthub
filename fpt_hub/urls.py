e009353c38672b41c55e3772bdbe12aaa15f781f
noproblem2311 <quanpva.dev@gmail.com>
2022-08-10 21:34:35 +0700

    updae

--- fpt_hub/urls.py
+++ fpt_hub/urls.py
@@ -20,18 +20,18 @@ from rest_framework import permissions
 # from drf_yasg.views import get_schema_view
 # from drf_yasg import openapi
 
-schema_view = get_schema_view(
-   openapi.Info(
-      title="FPT-HUB API",
-      default_version='v1',
-      description="Test description",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="quanpva.dev@gmail.com"),
-      license=openapi.License(name="Phung Vu An Quan"),
-   ),
-   public=True,
-   permission_classes=[permissions.AllowAny],
-)
+# schema_view = get_schema_view(
+#    openapi.Info(
+#       title="FPT-HUB API",
+#       default_version='v1',
+#       description="Test description",
+#       terms_of_service="https://www.google.com/policies/terms/",
+#       contact=openapi.Contact(email="quanpva.dev@gmail.com"),
+#       license=openapi.License(name="Phung Vu An Quan"),
+#    ),
+#    public=True,
+#    permission_classes=[permissions.AllowAny],
+# )
 
 urlpatterns = [
     # re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
